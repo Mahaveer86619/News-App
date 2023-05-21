ca053275a373e55cce520dadeb11bb50b733d851
Mahaveer prasad panigrahi <psatyam86619@gmail.com>
2023-05-21 22:40:56 +0530

    last commit, api may get depreciated

--- app/src/main/java/com/example/newsapp/Adapter/NewsItemClicked.java
+++
@@ -1,5 +0,0 @@
-package com.example.newsapp.Adapter;
-
-public interface NewsItemClicked {
-    public void onItemClicked(String item);
-}
---
+++ app/src/main/java/com/example/newsapp/Data_class/News_data_class.java
@@ -0,0 +1,2 @@
+package com.example.newsapp.Data_class;public class News_data_class {
+}
